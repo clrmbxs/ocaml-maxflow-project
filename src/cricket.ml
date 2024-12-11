2a4b9ee1c36df7f0f4a122bb68f8e4eae8af2230
Fornmat <matthias.fornaro@gmail.com>
2024-12-11 10:47:33 +0100

    teams to node

--- src/cricket.ml
+++ src/cricket.ml
@@ -13,14 +13,14 @@ type matchs = {
   remain: int
 } 
 
-let create_graph t tlist = 
+let create_graph t teams lmatchs = 
   let max_win_t = t.wins + t.g_left in
-  let rec f gr =
+  let rec f gr teams1 id =
 
-  match tlist with
+  match teams1 with
   | [] -> gr
-  | team :: rest -> if team.name!=t.name then gr
+  | team :: rest -> if team.name!=t.name then f (new_node gr (id)) rest (id+1) else f gr rest (id)
 
 
   in
-  f empty_graph
+  f empty_graph teams 2
